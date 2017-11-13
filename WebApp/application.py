01e6998d9ecceaf8ccd88853e6865bdbb485ba42
Namir Fawaz <namirfa@gmail.com>
2017-11-12 23:35:43 -0500

    made the stream work with the raspberry pi, added some styling

--- WebApp/application.py
+++ WebApp/application.py
@@ -1,32 +1,10 @@
-
 from flask import Flask, render_template, Response, request
 application = Flask(__name__)
 app = application # AWS Beanstalk requires it to be called application, but we can just use app for everything else because it's shorter
 
-from importlib import import_module
-Camera = import_module('camera_opencv').Camera
-
 @app.route("/")
 def home():
-	displayVideo = False
-	if request.args.get('displayVideo') and request.args.get('displayVideo') == "True":
-		displayVideo = True
-
-	return render_template('home.html', displayVideo=displayVideo)
-
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()

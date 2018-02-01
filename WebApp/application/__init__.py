3806a45c00515806ea361e2dd8d8789ada429564
Brandon Webster <bwebster496@gmail.com>
2018-01-31 21:37:01 -0500

    set database back to AWS RDS and got it working. RDS is set to accept MySQL requests from anywhere, sooo might want to fix that at some point

--- WebApp/application/__init__.py
+++ WebApp/application/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #'mysql+pymysql://buterrier:ConshredSpybot@seniordesign-dbinstance.cqwafy63ykmq.us-east-1.rds.amazonaws.com/flaskdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buterrier:ConshredSpybot@seniordesign-dbinstance.cqwafy63ykmq.us-east-1.rds.amazonaws.com/flaskdb'
 db = SQLAlchemy(app)
+
+# for local db: 'sqlite:///test.db'
